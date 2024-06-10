e45e459c10c635416d662a80dcfe339a1a1ffc20
JonathanLL2811 <156973028+JonathanLL2811@users.noreply.github.com>
2024-06-10 14:30:34 -0600

    ver publicaciones

--- publicaciones.js
+++ publicaciones.js
@@ -1,6 +1,6 @@
-// publicaciones.js
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const pool = require('./conexion');
 const router = express.Router();
 
@@ -11,8 +11,13 @@ const upload = multer({ storage: storage });
 router.get('/', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM tbl_publicaciones');
-    res.json(result.rows);
+    const publicaciones = result.rows.map(pub => ({
+      ...pub,
+      foto: pub.foto ? pub.foto.toString('base64') : null // Convertir buffer a base64 si existe
+    }));
+    res.json(publicaciones);
   } catch (error) {
+    console.error('Error al obtener publicaciones:', error);
     res.status(500).json({ error: error.message });
   }
 });
@@ -21,18 +26,26 @@ router.get('/', async (req, res) => {
 router.post('/', upload.single('foto'), async (req, res) => {
   try {
     const { descripcion, nombre_usuario } = req.body;
-    const foto = req.file ? req.file.buffer : null;
+    const fotoBuffer = req.file ? req.file.buffer : null;
     const nombre_foto = req.file ? req.file.originalname : null;
     const mime_type = req.file ? req.file.mimetype : null;
 
-    const query = `
-      INSERT INTO tbl_publicaciones (descripcion, nombre_usuario, foto, nombre_foto, mime_type)
-      VALUES ($1, $2, $3, $4, $5) RETURNING *;
-    `;
-    const values = [descripcion, nombre_usuario, foto, nombre_foto, mime_type];
-    const result = await pool.query(query, values);
+    if (fotoBuffer) {
+      const resizedBuffer = await sharp(fotoBuffer)
+        .resize({ width: 300 }) // Ajusta el ancho según tus necesidades
+        .toBuffer();
 
-    res.json(result.rows[0]);
+      const query = `
+        INSERT INTO tbl_publicaciones (descripcion, nombre_usuario, foto, nombre_foto, mime_type)
+        VALUES ($1, $2, $3::BYTEA, $4, $5) RETURNING *;
+      `;
+      const values = [descripcion, nombre_usuario, resizedBuffer, nombre_foto, mime_type];
+      const result = await pool.query(query, values);
+
+      res.json(result.rows[0]);
+    } else {
+      res.status(400).json({ error: 'No se proporcionó una imagen' });
+    }
   } catch (error) {
     console.error('Error al crear publicación:', error);
     res.status(500).json({ error: error.message });
@@ -40,3 +53,4 @@ router.post('/', upload.single('foto'), async (req, res) => {
 });
 
 module.exports = router;
+
